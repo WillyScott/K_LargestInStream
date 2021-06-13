250d31ca87148bff93fe8b4e922cbf7a87313b52
William Gromme <wgromme@yahoo.com>
2021-06-13 13:39:46 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace K_LargestInStream
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

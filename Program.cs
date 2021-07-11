6f4ccf730f0f34881042993cc1c521799bdb44d3
William Gromme <wgromme@yahoo.com>
2021-07-11 19:53:16 -0400

    Complete, insert values in O(logN).

--- Program.cs
+++ Program.cs
@@ -9,28 +9,33 @@ namespace K_LargestInStream
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            //int[] temp = { 4, 5, 8, 2 };
+            Console.WriteLine("Find the k largest in stream!");
+            int[] temp = { 4, 5, 8, 2 };
 
-            //KthLargest kth = new KthLargest(3, temp);
-            //Console.WriteLine(kth.Add(3));
-            //Console.WriteLine(kth.Add(5));
-            //Console.WriteLine(kth.Add(10));
-            //Console.WriteLine(kth.Add(9));
-            //Console.WriteLine(kth.Add(4));
+            Console.WriteLine("test 1");
+            KthLargest kth = new KthLargest(3, temp);
+            Console.WriteLine(kth.Add(3));
+            Console.WriteLine(kth.Add(5));
+            Console.WriteLine(kth.Add(10));
+            Console.WriteLine(kth.Add(9));
+            Console.WriteLine(kth.Add(4));
 
-            //// [[1,[]],[-3],[-2],[-4],[0],[4]]
-            //int[] temp2 = { };
-            //KthLargest kth2 = new KthLargest(1, temp2);
+            // [[1,[]],[-3],[-2],[-4],[0],[4]]
+            Console.WriteLine();
+            Console.WriteLine("test 2");
+            int[] temp2 = { };
+            KthLargest kth2 = new KthLargest(1, temp2);
 
-            //Console.WriteLine(kth2.Add(-3));
-            //Console.WriteLine(kth2.Add(-2));
-            //Console.WriteLine(kth2.Add(-4));
-            //Console.WriteLine(kth2.Add(0));
-            //Console.WriteLine(kth2.Add(4));
+            Console.WriteLine(kth2.Add(-3));
+            Console.WriteLine(kth2.Add(-2));
+            Console.WriteLine(kth2.Add(-4));
+            Console.WriteLine(kth2.Add(0));
+            Console.WriteLine(kth2.Add(4));
 
 
             //[[2,[0]],[-1],[1],[-2],[-4],[3]]
+            Console.WriteLine();
+            Console.WriteLine("test 3");
             int[] temp3 = { 0 };
             KthLargest kth3 = new KthLargest(2, temp3);
             Console.WriteLine(kth3.Add(-1));
@@ -76,8 +81,9 @@ namespace K_LargestInStream
                     else
                     {
                         // add value to end and sort
-                        listNumbers.Add(val);
-                        listNumbers.Sort();
+                        //listNumbers.Add(val);
+                        //listNumbers.Sort();
+                        sortList();
                         int temp = listNumbers.Count() - _k;
                         return listNumbers[temp];
                     }
@@ -87,20 +93,52 @@ namespace K_LargestInStream
             else
             {
                 int temp = listNumbers.Count() - _k;
-                if (val <= listNumbers[temp])
+                if (val < listNumbers[temp])  // no need to insert a value less than the Kth integer
                 {
                     return listNumbers[temp];
                 }
                 else  // need to insert the larger value and sort
                 {
-                    listNumbers.Add(val);
-                    listNumbers.Sort();
+
+                    sortList();
+                    //listNumbers.Add(val);
+                    //listNumbers.Sort();
                     temp = listNumbers.Count() - _k;
                     return listNumbers[temp];
 
                 }
 
             }
+
+            void sortList()
+            {
+                int startIndex = listNumbers.Count() - _k;
+                int endIndex = listNumbers.Count;
+               
+                while (endIndex > startIndex)
+                {
+                    int windowSize = endIndex - startIndex;
+                    int middleIndex = startIndex + windowSize / 2;
+                    // compare the value a middleIndex to the value being inserted "val"
+                    int compare = listNumbers[middleIndex].CompareTo(val); // return 0 greater 0, lessthan 0
+                    if (compare == 0)
+                    {
+                        listNumbers.Insert(middleIndex, val);
+                        return;
+                    }
+                    else if (compare < 0)
+                    {
+                        startIndex = middleIndex + 1;
+                    }
+                    else // >0
+                    {
+                        endIndex = middleIndex;
+                    }
+
+                }
+                listNumbers.Insert(startIndex, val);
+                
+             }
         }
     }
 }
